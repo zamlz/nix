c5a0eedb769983ba9ae52ef77e89de3681cc1942
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2024-05-16 00:34:01 -0700

    Added various more helper functions to xorg utility

--- scripts/xorg.py
+++ scripts/xorg.py
@@ -3,13 +3,14 @@ import subprocess
 from dataclasses import dataclass
 from enum import StrEnum
 from pathlib import Path
-from typing import Dict, Optional
+from typing import Dict, List, Optional
 
 import ps
 from colors import AnsiColor
 
 
 WINDOW_ID_ENV_DIR = Path("/tmp/.wid")
+LAST_FOCUSED_WINDOW_ID_FILE = Path("/tmp/.last_focused_wid")
 WINDOW_PWD_REGEX = re.compile("^WINDOW_PWD='(.+)'$")
 
 
@@ -74,10 +75,30 @@ def get_pwd_of_window(window_id: int) -> Optional[Path]:
         for line in f.readlines():
             match = re.match(WINDOW_PWD_REGEX, line)
             if match is not None:
-                return match.group(1)
+                return Path(match.group(1))
     raise ValueError(
         f"Found window id file for {window_id} but no pwd was found!"
     )
+
+
+def get_last_focused_window_id() -> Optional[int]:
+    if not LAST_FOCUSED_WINDOW_ID_FILE.exists():
+        return None
+    with open(LAST_FOCUSED_WINDOW_ID_FILE, 'r') as f:
+        return int(f.read().strip())
+
+
+# the filesystem pointer is a special construct for keeping track of
+# working directories in active windows
+def get_filesystem_pointer(global_search_mode: bool) -> Path:
+    if global_search_mode:
+        return Path.home()
+    last_focused_window_id = get_last_focused_window_id()
+    if last_focused_window_id is None:
+        return Path.home()
+    window_pwd = get_pwd_of_window(last_focused_window_id)
+    # FIXME: convert it to git path
+    return window_pwd
 
 
 def get_active_windows() -> Dict[int ,XorgWindow]:
@@ -163,5 +184,20 @@ def set_window_title(title: str) -> None:
     print(f'\33]0;{title}\a', end='', flush=True)
 
 
+class SearchMode(StrEnum):
+    FILE = 'f'
+    DIRECTORY = 'd'
+
+
+def get_dir_items(root_dir: Path, mode: SearchMode) -> List[str]:
+    result = subprocess.run(
+        ["find", str(root_dir), "-not", "-path", "'*/.*'", "-type", mode],
+        capture_output=True
+    )
+    result.check_returncode()
+    paths = str(result.stdout, encoding="utf-8").strip().split("\n")
+    return [p.replace(str(root_dir), ".") for p in paths]
+
+
 if __name__ == "__main__":
-    print(get_active_windows())
+    print(get_dir_items(Path.home(), SearchMode.DIRECTORY))
