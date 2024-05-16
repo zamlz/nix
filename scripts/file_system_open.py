d2bb3031b14c9d2de2caab3f342a47de121d13e7
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2024-05-16 00:34:21 -0700

    Added a wip script for file system open

---
+++ scripts/file_system_open.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import argparse
+import site
+import sys
+from pathlib import Path
+
+# FIXME: I have utils that are not installed as python packages yet.
+# Eventually, I should consolidate this and create a mechanism in nix to do so.
+site.addsitedir(str(Path(__file__).parent))
+
+import ps
+import xorg
+from fzf import Fzf
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file", action="store_true")
+    parser.add_argument("-d", "--directory", action="store_true")
+    parser.add_argument("-g", "--global-search", action="store_true")
+    args = parser.parse_args()
+    if args.file == args.directory:
+        raise ValueError("Mode must be -f or -d")
+
+    filesystem_pointer = xorg.get_filesystem_pointer(args.global_search)
+    fzf = Fzf(
+        prompt=f"Open {'File' if args.file else 'Directory'}:",
+        header=f"(Currently in {filesystem_pointer})",
+        preview=(
+            str(Path(__file__).parent / "fzf-file-preview.sh") \
+            + f" {filesystem_pointer}{{}}"
+        )
+    )
+
+    selection = fzf.prompt(xorg.get_dir_items(
+        filesystem_pointer,
+        xorg.SearchMode.FILE if args.file else xorg.SearchMode.DIRECTORY
+    ))
+
+    if selection == "":
+        sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
