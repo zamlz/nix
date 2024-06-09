ea3a391e0dc447d0c7d3de5a61f2ea05bcd2c0a7
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2024-06-09 16:57:00 -0700

    added a job manager for zsh using fzf. could be cool

--- shell/zsh.nix
+++ shell/zsh.nix
@@ -112,6 +112,25 @@
           # FIXME: does not work with fg well
           printf "\033]0;%s\a" "$1"
       }
+
+      # FIXME: Eventually make this a widget like the fzf-zsh-history plugin (ctrl-r)
+      fzf-foreground-job() {
+          [ -z "$(jobs)" ] && fg && return;
+          fg %$(jobs | fzf --bind 'enter:become(echo %{1})' | tr -d '[]')
+      }
+      bindkey -s "^F" 'fzf-foreground-job^M'
+
+      autobg-ctrl-b () {
+        if [[ $#BUFFER -eq 0 ]]; then
+          echo ""
+          bg
+          zle redisplay
+        else
+          zle push-input
+        fi
+      }
+      zle -N autobg-ctrl-b
+      bindkey '^B' autobg-ctrl-b
     '';
   };
 }
