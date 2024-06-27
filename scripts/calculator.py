88ca376a849799d98e81f57518a76e56cec39883
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2024-06-27 10:19:50 -0700

    added some improvements to calculator script

--- scripts/calculator.py
+++ scripts/calculator.py
@@ -7,9 +7,11 @@ import logging
 import operator
 import site
 import sys
+from dataclasses import dataclass
 from enum import StrEnum
 from functools import reduce
 from pathlib import Path
+from typing import Optional
 
 # FIXME: I have utils that are not installed as python packages yet.
 # Eventually, I should consolidate this and create a mechanism in nix to do so.
@@ -17,6 +19,7 @@ site.addsitedir(str(Path(__file__).parent))
 
 import navi.system
 from navi.logging import setup_logger
+from navi.shell.colors import AnsiColor
 from navi.shell.fzf import Fzf
 from navi.xorg.window import set_window_title
 
@@ -25,14 +28,25 @@ logger = logging.getLogger(__name__)
 
 
 class Operation(StrEnum):
-    NOP = "nop"
-    ADD = "add"
-    SUBTRACT = "sub"
-    MULTIPLY = "mul"
-    DIVIDE = "div"
-    POWER = "pow"
-    PERCENT = "per"
-    FACTORIAL = "fac"
+    NOP = "no-operation"
+    ADD = "addition"
+    SUBTRACT = "subtract"
+    MULTIPLY = "multiply"
+    DIVIDE = "divide"
+    POWER = "power"
+    PERCENT = "percent"
+    FACTORIAL = "factorial"
+
+
+operation_symbol_map = {
+    Operation.ADD: '+',
+    Operation.SUBTRACT: '-',
+    Operation.MULTIPLY: '*',
+    Operation.DIVIDE: '/',
+    Operation.POWER: '^',
+    Operation.PERCENT: '%',
+    Operation.FACTORIAL: '!'
+}
 
 
 binary_operation_map = {
@@ -45,8 +59,28 @@ binary_operation_map = {
 
 unary_operation_map = {
     Operation.PERCENT: lambda x: x / 100,
-    Operation.FACTORIAL: lambda x: reduce(operator.mul, range(1, x+1))
+    Operation.FACTORIAL: lambda x: reduce(operator.mul, range(1, int(x)+1))
 }
+
+
+@dataclass(frozen=True)
+class NumericalValue:
+    value: float
+    history: Optional[str] = None
+
+    def __str__(self) -> str:
+        if self.history is None:
+            return str(self.value)
+        history = (
+            f"{AnsiColor.ITALIC}{AnsiColor.LIGHT_BLUE}"
+            f" = {self.history}{AnsiColor.RESET}"
+        )
+        return f"{self.value}{history}"
+
+    def get_history(self) -> str:
+        if self.history is None:
+            return str(self.value)
+        return self.history
 
 
 def main() -> None:
@@ -72,6 +106,7 @@ def main() -> None:
             disabled=True
         )
         selection = fzf.prompt(stack[::-1])[0]
+        print(selection)
         if selection == '':
             return
 
@@ -79,7 +114,7 @@ def main() -> None:
         operation = selection_split[0]
 
         try:
-            stack.append(float(selection_split[1]))
+            stack.append(NumericalValue(value=float(selection_split[1])))
         except IndexError:
             pass  # the index error can be silently ignored
         except ValueError as e:
@@ -91,16 +126,27 @@ def main() -> None:
                 error_msg = "ERROR: Not enough values in the stack!"
                 continue
             op = binary_operation_map[operation]
+            symbol = operation_symbol_map[operation]
             x1 = stack.pop()
             x2 = stack.pop()
-            stack.append(op(x2, x1))
+            result = NumericalValue(
+                value=op(x2.value, x1.value),
+                history=f"({x2.get_history()} {symbol} {x1.get_history()})"
+            )
+            stack.append(result)
 
         elif operation in unary_operation_map.keys():
             if len(stack) < 1:
                 error_msg = "ERROR: Stack is empty!"
                 continue
             op = unary_operation_map[operation]
-            stack.append(op(stack.pop()))
+            symbol = operation_symbol_map[operation]
+            x1 = stack.pop()
+            result = NumericalValue(
+                value=op(x1.value),
+                history=f"({x1.get_history()} {symbol})"
+            )
+            stack.append(result)
 
         error_msg = ""
 
