231a774ad7dcc0061b4921675876afaf27acbc18
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2024-08-01 23:56:25 -0700

    added back substring search and removed original rc file

--- home/etc/zsh/default.nix
+++ home/etc/zsh/default.nix
@@ -58,6 +58,7 @@ in {
       ./init/prompt.zsh
       ./init/hooks.zsh
       ./init/jobs.zsh
+      ./init/substring-search.zsh
       ./init/python-venv.zsh
     ];
   };
---
+++ home/etc/zsh/init/substring-search.zsh
@@ -0,0 +1,4 @@
+#!/usr/bin/env zsh
+
+bindkey -M vicmd 'k' history-substring-search-up
+bindkey -M vicmd 'j' history-substring-search-down
