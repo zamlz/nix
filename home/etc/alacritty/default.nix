befc9622338d21321c8a5eeeaa87afb7d62e5af0
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2025-02-08 13:22:13 -0800

    updated font size for system

--- home/etc/alacritty/default.nix
+++ home/etc/alacritty/default.nix
@@ -48,7 +48,7 @@ in {
       };
 
       font = {
-        size = 8.0 * systemConfig.fontScale;
+        size = 6.0 * systemConfig.fontScale;
         glyph_offset = {
           x = 0;
           y = 0;
