d1e1b0add57d671d2f9acc0beb88da92b75a6108
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2025-02-20 00:27:41 -0800

    fd should never see inside git directories

--- home/etc/fd.nix
+++ home/etc/fd.nix
@@ -2,5 +2,8 @@
   # A simple, fast and user-friendly alternative to 'find'
   programs.fd = {
     enable = true;
+    ignores = [
+      "*.git/"  # never ever touch anything in this directory
+    ];
   };
 }
