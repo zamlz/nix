865e4ee76bd5a093df0b909f5c69495d114fbc31
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2025-05-15 12:48:35 -0700

    solaris: Use taskwarrior v3

--- home/etc/taskwarrior.nix
+++ home/etc/taskwarrior.nix
@@ -1,6 +1,7 @@
 { inputs, lib, config, pkgs, systemConfig, ... }: {
   programs.taskwarrior = {
     enable = true;
+    package = pkgs.taskwarrior3;
     config = {
       confirmation = false;
     };
