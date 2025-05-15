fd0ed3bcc43765038ac2a92cb4bb189e95bac6ee
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2025-05-15 13:25:46 -0700

    solaris: Updated taskwarrior configuration

--- home/etc/taskwarrior.nix
+++ home/etc/taskwarrior.nix
@@ -2,8 +2,10 @@
   programs.taskwarrior = {
     enable = true;
     package = pkgs.taskwarrior3;
+    colorTheme = "dark-256";
     config = {
       confirmation = false;
+      color.calendar.weekend = "green";
     };
   };
 }
