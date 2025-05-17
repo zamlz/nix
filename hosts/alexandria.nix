c12c1d48cf1d4ed3a83a62fe9c75fff4648628e8
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2025-05-17 12:40:10 -0700

    solaris: forgot a semicolon

--- hosts/alexandria.nix
+++ hosts/alexandria.nix
@@ -24,7 +24,7 @@
   services.zfs = {
     enable = true;
     autoSnapshot.enable = true;
-  }
+  };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
