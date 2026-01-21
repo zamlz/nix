d609cbd4d168dc3a608f2220f157f5c83f1ddd92
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2026-01-20 18:15:32 -0800

    pruned unused pieces of the configuration

--- home/desktop/wayland/default.nix
+++ home/desktop/wayland/default.nix
@@ -12,9 +12,9 @@ in
   xdg.configFile."niri/config.kdl".source = ./niri.kdl;
 
   home.packages = with pkgs; [
-    hyprlax
+    # hyprlax
     swaybg
-    anyrun-provider # appears I need to allow this to be defined as well to get anyrun to work.
+    xwayland-satellite
     # noctalia-shell # not in current flake revision
   ];
 
@@ -41,36 +41,36 @@ in
   };
 
   # FIXME: Doesn't appear
-  programs.anyrun = {
-    enable = true;
-    config = {
-      x = {
-        fraction = 0.5;
-      };
-      y = {
-        fraction = 0.3;
-      };
-      width = {
-        fraction = 0.3;
-      };
-      hideIcons = false;
-      ignoreExclusiveZones = false;
-      layer = "overlay";
-      hidePluginInfo = false;
-      closeOnClick = false;
-      showResultsImmediately = false;
-      maxEntries = null;
-      plugins = [
-        "${pkgs.anyrun}/lib/libapplications.so"
-        # "${pkgs.anyrun}/lib/libsymbols.so"
-        # "${pkgs.anyrun}/lib/librink.so"
-        # "${pkgs.anyrun}/lib/libshell.so"
-        # "${pkgs.anyrun}/lib/libtranslate.so"
-        # "${pkgs.anyrun}/lib/librandr.so"
-        # "${pkgs.anyrun}/lib/libdictionary.so"
-        # "${pkgs.anyrun}/lib/libnix_run.so"
-        # "${pkgs.anyrun}/lib/libniri_focus.so"
-      ];
-    };
-  };
+  # programs.anyrun = {
+  #   enable = false;
+  #   config = {
+  #     x = {
+  #       fraction = 0.5;
+  #     };
+  #     y = {
+  #       fraction = 0.3;
+  #     };
+  #     width = {
+  #       fraction = 0.3;
+  #     };
+  #     hideIcons = false;
+  #     ignoreExclusiveZones = false;
+  #     layer = "overlay";
+  #     hidePluginInfo = false;
+  #     closeOnClick = false;
+  #     showResultsImmediately = false;
+  #     maxEntries = null;
+  #     plugins = [
+  #       "${pkgs.anyrun}/lib/libapplications.so"
+  #       # "${pkgs.anyrun}/lib/libsymbols.so"
+  #       # "${pkgs.anyrun}/lib/librink.so"
+  #       # "${pkgs.anyrun}/lib/libshell.so"
+  #       # "${pkgs.anyrun}/lib/libtranslate.so"
+  #       # "${pkgs.anyrun}/lib/librandr.so"
+  #       # "${pkgs.anyrun}/lib/libdictionary.so"
+  #       # "${pkgs.anyrun}/lib/libnix_run.so"
+  #       # "${pkgs.anyrun}/lib/libniri_focus.so"
+  #     ];
+  #   };
+  # };
 }
