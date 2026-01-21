6a843dbf9fa214c9a41c7b3d57d4bb9df7f28da4
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2026-01-20 18:01:52 -0800

    force herbstluftwm to be on tty2

--- home/cli/zsh/login.zsh
+++ home/cli/zsh/login.zsh
@@ -2,10 +2,6 @@
 
 # This file should contain anything we would normally put in `.zlogin`
 
-if [ -z "$DISPLAY" ] && [ "$(fgconsole 2>/dev/null)" -eq 1 ]; then
-    exec startx $HOME/.config/xinit/rc.sh "herbstluftwm"
-fi
-
 if [ -z "$DISPLAY" ] && [ "$(fgconsole 2>/dev/null)" -eq 2 ]; then
-    exec niri
+    exec startx $HOME/.config/xinit/rc.sh "herbstluftwm"
 fi
