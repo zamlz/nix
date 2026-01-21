3b4731f1d02c1c727867bf71c3439dc12b6d9aee
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2026-01-20 18:07:47 -0800

    Added swaylock

--- home/desktop/wayland/default.nix
+++ home/desktop/wayland/default.nix
@@ -2,18 +2,42 @@
   pkgs,
   ...
 }:
+let
+  removeHash = str: builtins.replaceStrings ["#"] [""] str;
+  colorScheme = (import ../../../lib/colorschemes.nix).defaultColorScheme;
+  noHashColorScheme = builtins.mapAttrs (name: value: removeHash value) colorScheme;
+in
 {
   # FIXME: use niri through niri-flakes for home manager support
   xdg.configFile."niri/config.kdl".source = ./niri.kdl;
 
   home.packages = with pkgs; [
-    hyprlax # potential wallpaper engine?
+    hyprlax
+    swaybg
+    anyrun-provider # appears I need to allow this to be defined as well to get anyrun to work.
     # noctalia-shell # not in current flake revision
   ];
 
   # FIXME: Need configuration
   programs.waybar = {
     enable = true;
+  };
+
+  programs.swaylock = {
+    enable = true;
+    settings = with noHashColorScheme; {
+      ignore-empty-password = true;
+      show-failed-attempts = true;
+      image = "~/.config/wallpaper";
+      scaling = "tile";
+      hide-keyboard-layout = true;
+      disable-caps-lock-text = true;
+      bs-hl-color = magenta;
+      font-size = 48;
+      indicator-idle-visible = false;
+      indicator-radius = 80;
+      indicator-thickness = 12;
+    };
   };
 
   # FIXME: Doesn't appear
@@ -38,14 +62,14 @@
       maxEntries = null;
       plugins = [
         "${pkgs.anyrun}/lib/libapplications.so"
-        "${pkgs.anyrun}/lib/libsymbols.so"
-        "${pkgs.anyrun}/lib/librink.so"
-        "${pkgs.anyrun}/lib/libshell.so"
-        "${pkgs.anyrun}/lib/libtranslate.so"
-        "${pkgs.anyrun}/lib/librandr.so"
-        "${pkgs.anyrun}/lib/libdictionary.so"
-        "${pkgs.anyrun}/lib/libnix_run.so"
-        "${pkgs.anyrun}/lib/libniri_focus.so"
+        # "${pkgs.anyrun}/lib/libsymbols.so"
+        # "${pkgs.anyrun}/lib/librink.so"
+        # "${pkgs.anyrun}/lib/libshell.so"
+        # "${pkgs.anyrun}/lib/libtranslate.so"
+        # "${pkgs.anyrun}/lib/librandr.so"
+        # "${pkgs.anyrun}/lib/libdictionary.so"
+        # "${pkgs.anyrun}/lib/libnix_run.so"
+        # "${pkgs.anyrun}/lib/libniri_focus.so"
       ];
     };
   };
