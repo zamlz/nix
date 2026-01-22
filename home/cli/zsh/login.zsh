6d17bf80299dd29fbf13efc780fa3bf616f6ca9e
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2026-01-21 20:19:01 -0800

    updated zlogin for the time being

--- home/cli/zsh/login.zsh
+++ home/cli/zsh/login.zsh
@@ -1,6 +1,9 @@
 #!/usr/bin/env zsh
 
 # This file should contain anything we would normally put in `.zlogin`
+if [ -z "$DISPLAY" ] && [ "$(fgconsole 2>/dev/null)" -eq 1 ]; then
+    exec niri
+fi
 
 if [ -z "$DISPLAY" ] && [ "$(fgconsole 2>/dev/null)" -eq 2 ]; then
     exec startx $HOME/.config/xinit/rc.sh "herbstluftwm"
