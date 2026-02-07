6b245655b67a26df7e6002cbd81a6df9a9d4d097
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2026-02-07 13:49:23 -0800

    updated default location of password store

--- home/cli/password-store.nix
+++ home/cli/password-store.nix
@@ -8,7 +8,7 @@
     enable = true;
     package = pkgs.pass.withExtensions (exts: [ exts.pass-update ]);
     settings = {
-      PASSWORD_STORE_DIR = "${config.xdg.configHome}/password-store";
+      PASSWORD_STORE_DIR = "${config.home.homeDirectory}/usr/passwords";
     };
   };
 }
