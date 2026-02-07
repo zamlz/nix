533a73e4fc69c3e19d4b34feacf74e10ab006ece
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2026-02-07 13:00:03 -0800

    Generate better error handling on process errors

--- packages/navi-scripts/src/navi/logging.py
+++ packages/navi-scripts/src/navi/logging.py
@@ -1,6 +1,7 @@
 import sys
 from functools import wraps
 from pathlib import Path
+from subprocess import CalledProcessError
 from typing import Callable, TypeVar
 
 from loguru import logger
@@ -10,6 +11,18 @@ LOG_DIR = Path.home() / ".local/share/navi"
 LOG_FILE = LOG_DIR / "navi_system.log"
 
 F = TypeVar("F", bound=Callable[..., None])
+
+
+def _log_process_errors(e: CalledProcessError) -> None:
+    logger.error(f"Command failed: \n{e.cmd}")
+    logger.error(f"Command stdout: \n{e.stdout}")
+    logger.error(f"Command stderr: \n{e.stderr}")
+
+
+def _pause_and_exit_on_error(e: Exception) -> None:
+    logger.exception(f"Exception encountered: {e}")
+    input()
+    sys.exit(1)
 
 
 def setup_main_logging(func: F) -> F:
@@ -22,9 +35,10 @@ def setup_main_logging(func: F) -> F:
     def wrapped_func(*args, **kwargs):
         try:
             return func(*args, **kwargs)
+        except CalledProcessError as e:
+            _log_process_errors(e)
+            _pause_and_exit_on_error(e)
         except Exception as e:
-            logger.exception(f"Exception encountered: {e}")
-            input()
-            sys.exit(1)
+            _pause_and_exit_on_error(e)
 
     return wrapped_func  # type: ignore[return-value]
