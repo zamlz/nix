8b84dbc4cd64239ee708e8200a58db1d7870abe3
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2026-02-17 11:26:44 -0800

    Revert "added home manager session variables to systemd user services"
    This reverts commit d9abd892019cd59773b1ae6a93a04e5605cdd56c.

--- home/cli/systemd.nix
+++ home/cli/systemd.nix
@@ -1,14 +1,7 @@
-{
-  config,
-  ...
-}:
-{
+_: {
   systemd.user = {
     enable = true;
     startServices = "sd-switch";
-
-    # Import home.sessionVariables into systemd user environment
-    sessionVariables = config.home.sessionVariables;
 
     # Systemd task to clean out my tmp directory if a file gets over a week old
     tmpfiles.rules = [
