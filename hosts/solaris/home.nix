5b1d8d78ac1570c74b29e060955047915122f954
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2026-02-18 16:51:34 -0800

    fixed solaris screen resolution

--- hosts/solaris/home.nix
+++ hosts/solaris/home.nix
@@ -11,7 +11,16 @@
 
   my.fontScale = 2.0;
   # FIXME: ultrawide monitor is too large so let's make this a bit more sane.
-  programs.niri.settings.layout.default-column-width.proportion = 0.5;
+  programs.niri.settings = {
+    layout.default-column-width.proportion = 0.5;
+    outputs.DP-4 = {
+      mode = {
+        width = 5120;
+        height = 1440;
+        refresh = 119.970;
+      };
+    };
+  };
 
   home = {
     username = "amlesh";
