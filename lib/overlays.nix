5d9cf3dcbeeb49c33d668d882be0466fcca62775
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2026-02-28 16:55:20 -0800

    updated caddy cloudflare hash

--- lib/overlays.nix
+++ lib/overlays.nix
@@ -6,7 +6,7 @@
     navi-scripts = final.callPackage (self + /packages/navi-scripts/package.nix) { };
     caddy-with-cloudflare = final.caddy.withPlugins {
       plugins = [ "github.com/caddy-dns/cloudflare@v0.2.3" ];
-      hash = "sha256-bJO2RIa6hYsoVl3y2L86EM34Dfkm2tlcEsXn2+COgzo=";
+      hash = "sha256-mmkziFzEMBcdnCWCRiT3UyWPNbINbpd3KUJ0NMW632w=";
     };
   })
 ]
