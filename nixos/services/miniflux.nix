10776af98fe79b4c1658ab12cf7a0e9ec16425a7
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2026-03-01 20:23:26 -0800

    added a comment for oidx miniflux

--- nixos/services/miniflux.nix
+++ nixos/services/miniflux.nix
@@ -41,6 +41,7 @@
       OAUTH2_PROVIDER = "oidc";
       OAUTH2_CLIENT_ID = "50c4a367-91d7-48a5-9c71-968b0f51e267";
       OAUTH2_REDIRECT_URL = "https://miniflux.${constants.domainSuffix}/oauth2/oidc/callback";
+      # Base URL only — go-oidc appends /.well-known/openid-configuration automatically
       OAUTH2_OIDC_DISCOVERY_ENDPOINT = "https://pocket-id.${constants.domainSuffix}";
       OAUTH2_USER_CREATION = "1";
     };
